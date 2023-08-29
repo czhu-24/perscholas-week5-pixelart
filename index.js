9c52e4d8b8c4176351d35085c5dcee05b50a5790
czhu-24 <blue.ink.stains@gmail.com>
2023-08-29 10:12:54 -0700

    added vector icons and fixed up the pencil drawing

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let currentColor = "black";
 let backgroundColor = "white"; // color used to simulate "erasing"
-let currentMode = "pen";
+let currentMode = "pencil";
 let toolSize = 10;
 
 let isMoving = false; // boolean to handle tracking movement of mouse during 
 // drawing & erasing
 
 const canvas = document.querySelector("canvas");
-const colorPicker = document.getElementById("color-picker");
-const penPicker = document.getElementById("pen-square");
-const eraserPicker = document.getElementById("eraser-square");
 
+const colorPicker = document.getElementById("color-picker");
+const pencilPicker = document.getElementById("pencil-square");
+const eraserPicker = document.getElementById("eraser-square");
+const rectanglePicker = document.getElementById("rectangle-square");
 const resetPicker = document.getElementById("reset-square");
+
+// for debugging
+const currentToolText = document.getElementById("current-tool");
 
 const ctx = canvas.getContext("2d");
 const rect = canvas.getBoundingClientRect(); // Get canvas's position on the page
@@ -20,14 +24,19 @@ const rect = canvas.getBoundingClientRect(); // Get canvas's position on the pag
 
 const canvasMousedown = (e) => {
     isMoving = true;
+
     const x = e.clientX - rect.left; // Cursor's x-coordinate relative to the canvas
     const y = e.clientY - rect.top; // Cursor's y-coordinate relative to the canvas
 
     ctx.lineWidth = toolSize;
 
     switch(currentMode){
-        case("pen"):
+        case("pencil"):
             ctx.strokeStyle = currentColor;
+            ctx.beginPath(); 
+            ctx.moveTo(x,y);
+            break;
+        case("rectangle"):
             break;
         case("eraser"):
             ctx.fillStyle = backgroundColor;
@@ -43,26 +52,26 @@ const canvasMousemove = (e) => {
     if(!isMoving){
         return;
     }
-    const x = e.clientX - rect.left; // Cursor's x-coordinate relative to the canvas
-    const y = e.clientY - rect.top; // Cursor's y-coordinate relative to the canvas
+    const x = e.clientX - rect.left; 
+    const y = e.clientY - rect.top; 
 
     switch(currentMode){
-        case("pen"):
+        case("pencil"):
             ctx.lineTo(x, y);
             break;
         case("eraser"):
             ctx.fillRect(x, y, toolSize, toolSize);
             break;
     }
-    canvas.addEventListener("mouseup", (e) => canvasMouseup(e));
+
 };
 
 const canvasMouseup = (e) => {
-    const x = e.clientX - rect.left; // Cursor's x-coordinate relative to the canvas
-    const y = e.clientY - rect.top; // Cursor's y-coordinate relative to the canvas
+    const x = e.clientX - rect.left; 
+    const y = e.clientY - rect.top; 
 
     switch(currentMode){
-        case("pen"):
+        case("pencil"):
             ctx.stroke();
             break;
         case("eraser"):
@@ -73,7 +82,6 @@ const canvasMouseup = (e) => {
 
 
 colorPicker.addEventListener("input", (e) => {
-    console.log(e.target.value);
     currentColor = e.target.value;
 });
 
@@ -82,12 +90,19 @@ canvas.addEventListener("mousemove", canvasMousemove);
 canvas.addEventListener("mouseup", canvasMouseup);
 
 
-penPicker.addEventListener("click", () => {
-    currentMode = "pen";
+pencilPicker.addEventListener("click", (e) => {
+    currentMode = "pencil";
+    currentToolText.textContent = currentMode; // TO REMOVE
 });
 
-eraserPicker.addEventListener("click", () => {
+eraserPicker.addEventListener("click", (e) => {
     currentMode = "eraser";
+    currentToolText.textContent = currentMode; // TO REMOVE
+});
+
+rectanglePicker.addEventListener("click", (e) => {
+    currentMode = "rectangle";
+    currentToolText.textContent = currentMode; // TO REMOVE
 });
 
 resetPicker.addEventListener("click", () => {
